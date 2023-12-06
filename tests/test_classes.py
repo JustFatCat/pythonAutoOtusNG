ff8f4f4c60131b21ebb2d7573d5dadb40e511bae
NatalisGrigoreva <natalis200623@gmail.com>
2023-12-06 23:14:10 +0300

    Correcting mistakes in HW2

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -6,9 +6,8 @@ import pytest
 
 @pytest.mark.parametrize(("side_a", "side_b", "area", "perimeter"),
                          [(4, 6, 24, 20),
-                          (5, 10, 50, 30),
-                          (6, 9, 54, 30)],
-                         ids=["set of positive numbers #1", "set of positive numbers #2", "set of positive numbers #3"])
+                          (5.6, 11.3, 63.28, 33.8)],
+                         ids=["set of positive integers", "set of positive floats"])
 def test_rectangle(side_a, side_b, area, perimeter):
     r = Rectangle(side_a, side_b)
     assert r.name == f"Rectangle {side_a} and {side_b}"
@@ -16,24 +15,18 @@ def test_rectangle(side_a, side_b, area, perimeter):
     assert r.get_perimeter() == perimeter
 
 
-@pytest.mark.parametrize(("side_a", "side_b", "area", "perimeter"),
-                         [(-4, -6, 24, -20),
-                          (-5, -10, 50, -30),
-                          (0, 0, 0, 0)],
-                         ids=["set of negative numbers #1", "set of negative numbers #2", "zero set"])
-def test_rectangle_negative(side_a, side_b, area, perimeter):
+@pytest.mark.parametrize(("side_a", "side_b"),
+                         [(-4, -6),
+                          (0, 0)],
+                         ids=["set of negative integers", "zero set"])
+def test_rectangle_negative(side_a, side_b):
     with pytest.raises(ValueError):
         r = Rectangle(side_a, side_b)
-        assert r.name == f"Rectangle {side_a} and {side_b}"
-        assert r.get_area() == area
-        assert r.get_perimeter() == perimeter
-
 
 @pytest.mark.parametrize(("side", "area", "perimeter"),
                          [(4, 16, 16),
-                          (5, 25, 20),
-                          (6, 36, 24)],
-                         ids=["set of positive numbers #1", "set of positive numbers #2", "set of positive numbers #3"])
+                          (5.7, 32.49, 22.8)],
+                         ids=["set of positive integers", "set of positive floats"])
 def test_square(side, area, perimeter):
     r = Square(side)
     assert r.name == f"Square {side}"
@@ -41,24 +34,19 @@ def test_square(side, area, perimeter):
     assert r.get_perimeter() == perimeter
 
 
-@pytest.mark.parametrize(("side", "area", "perimeter"),
-                         [(-4, 16, -20),
-                          (-5, 25, 20),
-                          (-6, 36, -24)],
-                         ids=["set of negative numbers #1", "set of negative numbers #2", "set of negative numbers #3"])
-def test_square_negative(side, area, perimeter):
+@pytest.mark.parametrize("side",
+                         [-4,
+                          0],
+                         ids=["negative integer", "zero"])
+def test_square_negative(side):
     with pytest.raises(ValueError):
         r = Square(side)
-        assert r.name == f"Square {side}"
-        assert r.get_area() == area
-        assert r.get_perimeter() == perimeter
 
 
 @pytest.mark.parametrize(("side_a", "side_b", "side_c", "area", "perimeter"),
                          [(3, 4, 5, 18, 12),
-                          (8, 6, 4, 67.5, 18),
-                          (10, 10, 6, 409.5, 26)],
-                         ids=["set of positive numbers #1", "set of positive numbers #2", "set of positive numbers #3"])
+                          (8.2, 4.2, 5.2, 43.71839999999994, 17.599999999999998)],
+                         ids=["set of positive integers", "set of positive floats"])
 def test_triangle(side_a, side_b, side_c, area, perimeter):
     tr = Triangle(side_a, side_b, side_c)
     assert tr.name == f"Triangle {side_a} and {side_b} and {side_c}"
@@ -66,40 +54,31 @@ def test_triangle(side_a, side_b, side_c, area, perimeter):
     assert tr.get_perimeter() == perimeter
 
 
-@pytest.mark.parametrize(("side_a", "side_b", "side_c", "area", "perimeter"),
-                         [(-3, -4, -5, 18, -12),
-                          (0, 0, 0, 0, 0),
-                          (-10, 10, -6, -409.5, 26)],
-                         ids=["set of negative numbers #1", "zero set", "set of negative numbers #2"])
-def test_triangle_negative(side_a, side_b, side_c, area, perimeter):
+@pytest.mark.parametrize(("side_a", "side_b", "side_c"),
+                         [(-3, -4, -5),
+                          (0, 0, 0)],
+                         ids=["set of negative integers", "zero set"])
+def test_triangle_negative(side_a, side_b, side_c):
     with pytest.raises(ValueError):
         tr = Triangle(side_a, side_b, side_c)
-        assert tr.name == f"Triangle {side_a} and {side_b} and {side_c}"
-        assert tr.get_area() == area
-        assert tr.get_perimeter() == perimeter
 
 @pytest.mark.parametrize(("radius", "area", "perimeter"),
                          [(11, 379.94, 69.08),
-                         (7, 153.86, 43.96),
-                         (3, 28.26, 18.84)],
-                         ids=["set of positive numbers #1", "set of positive numbers #2", "set of positive numbers #3"])
+                         (7.8, 191.0376, 48.984)],
+                         ids=["set of positive integers", "set of positive floats"])
 def test_circle(radius, area, perimeter):
     cr = Circle(radius)
     assert cr.name == f"Circle {radius}"
     assert cr.get_area() == area
     assert cr.get_perimeter() == perimeter
 
-@pytest.mark.parametrize(("radius", "area", "perimeter"),
-                         [(-11, -379.94, -69.08),
-                          (0, 0, 0),
-                          (-3, -28.26, -18.84)],
-                         ids=["set of negative numbers #1", "zero set", "set of negative numbers #2"])
-def test_circle_negative(radius, area, perimeter):
+@pytest.mark.parametrize("radius",
+                         [-11,
+                          0],
+                         ids=["negative integer", "zero"])
+def test_circle_negative(radius):
     with pytest.raises(ValueError):
         cr = Circle(radius)
-        assert cr.name == f"Circle {radius}"
-        assert cr.get_area() == area
-        assert cr.get_perimeter() == perimeter
 
 def test_add_area():
     r = Rectangle(2, 5)
